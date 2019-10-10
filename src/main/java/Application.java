62ff2d6fd3bd99daa7dbdffd87fd429db3101794
Erik cruz <saimiri.guias@gmail.com>
2019-10-10 10:01:57 -0500

    Commit inicial

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello mundo");
+        System.out.println(3*4);
+        System.out.println("suma: " + 2+8);
+
+    }
+}
